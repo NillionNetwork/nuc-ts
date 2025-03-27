9da422e4fda89f7d4b2ee1955f02856108dd7290
Pablo Hernández <pablo.hernandez@nillion.com>
2025-03-27 13:17:51 +0000

    chore: add code to check that containers are up

--- tests/fixture/global-setup.ts
+++ tests/fixture/global-setup.ts
@@ -1,10 +1,26 @@
 import dockerCompose from "docker-compose";
 
+const MAX_RETRIES = 300;
+
 export async function setup() {
   console.log("🚀 Starting containers...");
   try {
     await dockerCompose.upAll({ cwd: "./docker" });
-    // This gives time that all services are up
+    let retry = 0;
+    for (; retry < MAX_RETRIES; retry++) {
+      const result = await dockerCompose.ps({ cwd: "./docker" });
+      if (
+        result.data.services.every((service) => service.state.includes("Up"))
+      ) {
+        break;
+      }
+      await new Promise((f) => setTimeout(f, 200));
+    }
+    if (retry >= MAX_RETRIES) {
+      console.error("❌ Error starting containers: timeout");
+      process.exit(1);
+    }
+    // We need sleep 1 sec to be sure that the AboutResponse.started is at least 1 sec earlier than the tests start.
     await new Promise((f) => setTimeout(f, 2000));
     console.log("✅ Containers started successfully.");
   } catch (error) {
