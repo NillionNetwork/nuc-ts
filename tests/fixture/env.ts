f985e91e26426e29da463301e6b46cc1bcec0764
Pablo Hernández <pablo.hernandez@nillion.com>
2025-04-23 14:45:28 +0100

    chore: use preconfigured nilchain docker image

--- tests/fixture/env.ts
+++ tests/fixture/env.ts
@@ -3,6 +3,5 @@ import process from "node:process";
 export const Env = {
   nilAuthUrl: process.env.NILLION_NILAUTH_URL ?? "",
   nilChainUrl: process.env.NILLION_NILCHAIN_JSON_RPC ?? "",
-  Payer: process.env.NILLION_NILCHAIN_PRIVATE_KEY_0 ?? "",
-  NilauthClient: process.env.NILLION_NILCHAIN_PRIVATE_KEY_1 ?? "",
+  NilauthClient: process.env.NILLION_NILCHAIN_PRIVATE_KEY_0 ?? "",
 };
