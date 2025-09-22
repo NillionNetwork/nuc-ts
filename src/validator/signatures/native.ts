15fb5c0986aafe03e89b7df0a8b6bff8be7cf2ec
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2025-09-22 12:56:41 +0100

    fix: replace toBytes with WebApi's TextEncoder

--- src/validator/signatures/native.ts
+++ src/validator/signatures/native.ts
@@ -1,4 +1,3 @@
-import { toBytes } from "@noble/hashes/utils";
 import { Did } from "#/core/did/did";
 import type { Nuc } from "#/nuc/envelope";
 
@@ -6,7 +5,7 @@ export const NATIVE_SIGNATURE_VERIFICATION_FAILED =
   "native signature verification failed";
 
 export function validateNativeSignature(nuc: Nuc): void {
-  const msg = toBytes(`${nuc.rawHeader}.${nuc.rawPayload}`);
+  const msg = new TextEncoder().encode(`${nuc.rawHeader}.${nuc.rawPayload}`);
   const parsedDid = nuc.payload.iss;
 
   if (!Did.validateSignature(parsedDid, msg, nuc.signature)) {
